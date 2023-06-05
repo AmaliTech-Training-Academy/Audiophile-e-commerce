4168c0a6ef014e737693f688cdc11f17626e5088
Millicent-Konadu <millicent.opoku@amalitech.com>
2023-06-05 09:32:59 +0000

    Edited cart model

--- starter-code/audiophile_ecommerce_website/ecommerce/models.py
+++ starter-code/audiophile_ecommerce_website/ecommerce/models.py
@@ -3,14 +3,14 @@ from django.db import models
 from django.utils.text import slugify
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    product_name = models.CharField(max_length=100)
     slug = models.SlugField(unique= True, blank=True)
     category = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     categoryImage = models.ImageField(upload_to ='product -images/')
     new = models.BooleanField(default=True)
-    features = models.CharField(max_length=200, blank=True)
+    features = models.TextField()
     includes = models.JSONField(default=list)
 
     def __str__(self):
@@ -24,19 +24,29 @@ class Category(models.Model):
         return self.name
 
 
-class Cart(models.Model):
-        products = models.CharField(max_length=100, blank=True)
-        quantity = models.IntegerField()
-        price = models.DecimalField(max_digits=10, decimal_places=2)
-        total_price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
-        price_per_unit = 10
+from django.db import models
 
-        def calculate_total_price(quantity, price):
-            return quantity * price
+
+
+class Cart(models.Model):
+    product_name = models.CharField(max_length=100, blank=True)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    price_per_unit = 10
+
+    def calculate_total_price(self):
+        self.total_price = self.quantity * self.price
+
+    def save(self, *args, **kwargs):
+        self.calculate_total_price()
+        super().save(*args, **kwargs)
+
+
 
 
 class CartItem(models.Model):
-    products = models.CharField(max_length=100, blank=True)
+    product_name = models.CharField(max_length=100, blank=True)
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateField(auto_now_add=True)
